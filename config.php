fe7a501cd6db7a1eecf3d9e36395d5b318deac5d
cat-bucket <gitbit843@gmail.com>
2024-09-07 19:49:44 +0800

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ define('USE_NODES_COUNT', 5000); // 一次轰炸时所用的最大接口节点�
 define('CONCURRENCY', 50); // 轰炸时HTTP最大并发连接数
 define('THREAD_POOL_SIZE', 20); // 执行 update-nodes 命令时的线程池大小
 
-// 检查是否已定义API Key和代理配置
+// 检查是否已定义API Key
 if (!defined('SHODAN_API_KEY')) {
     define('SHODAN_API_KEY', readline("输入Shodan API Key（可选）: "));
 }
@@ -17,8 +17,9 @@ if (!defined('ZOOMEYE_API_KEY')) {
 // 默认设置
 define('ZOOMEYE_PAGE_LIMIT', 50); // 更新ZoomEye时限制的页面数量
 
+// 修改代理配置，默认不使用代理
 if (!defined('PROXY')) {
-    define('PROXY', readline("输入代理配置（例如：127.0.0.1:7890 或 socks5h://127.0.0.1:10808，留空使用默认127.0.0.1:10808）: ") ?: '127.0.0.1:10808');
+    define('PROXY', readline("输入代理配置（留空以不使用代理）: ") ?: null);
 }
 
 // 默认设置
